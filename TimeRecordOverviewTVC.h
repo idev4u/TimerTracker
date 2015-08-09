fa38ca073936b603a4733b6fde33b27ba847111e
idev4u <us.idev.student@googlemail.com>
2015-08-09 19:58:08 +0200

    add the empty Tableview Controller

---
+++ TimeRecordOverviewTVC.h
@@ -0,0 +1,13 @@
+//
+//  TimeRecordOverviewTVC.h
+//  TimeTracker
+//
+//  Created by Norman Sutorius on 09.08.15.
+//  Copyright © 2015 Norman Sutorius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TimeRecordOverviewTVC : UITableViewController
+
+@end
